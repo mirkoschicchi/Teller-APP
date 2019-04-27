4857395bb5e0f959086beda524d9fe5b3decd139
Mirko Schicchi <mirko.schicchi@studenti.unitn.it>
2019-04-27 12:46:05 +0200

    Audio recording works

--- android/app/src/main/java/com/home/MainApplication.java
+++ android/app/src/main/java/com/home/MainApplication.java
@@ -3,6 +3,8 @@ package com.home;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.zmxv.RNSound.RNSoundPackage;
+import com.goodatlas.audiorecord.RNAudioRecordPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSoundPackage(),
+            new RNAudioRecordPackage(),
           new RNGestureHandlerPackage()
       );
     }
--- src/screens/RecordPage.js
+++ src/screens/RecordPage.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import {ScrollView, StyleSheet, Text, View, TextInput, Button, Image} from 'react-native';
+import AudioRecord from 'react-native-audio-record';
+import Permissions from 'react-native-permissions';
+import Sound from 'react-native-sound';
 
 
 import RoundButton from '../components/RoundButton.js';
@@ -9,56 +12,114 @@ export default class RecordPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isRecording: false,
-            isFinishRecorded: false,
-            isPlaying: false,
-            isPaused: false,
-            currentTime: 0,
-            audioLength: 0
+            audioFile: '',
+            recording: false,
+            loaded: false,
+            paused: true
         }
-        this.timer = null
     }
 
-    record = () => {
-        const { isPlaying } = this.state
-        if (isPlaying) {
-          this.stopPlaying()
-        }
+    async componentDidMount() {
+        await this.checkPermission();
     
-        this.prepareRecordingPath()
-        AudioRecorder.startRecording()
-        this.setState({
-          isPlaying: false,
-          isRecording: true,
-          isFinishRecorded: false,
-          audioLength: 0,
-          currentTime: 0
-        })
+        const options = {
+          sampleRate: 16000,
+          channels: 1,
+          bitsPerSample: 16,
+          wavFile: 'test.wav'
+        };
     
-        this.timer = setInterval(() => {
-          const time = this.state.currentTime + 1
-          this.setState({currentTime: time})
-          if (time === Constants.MAX_AUDIO_LENGTH) {
-            this.stopRecording()
+        AudioRecord.init(options);
+    
+        // AudioRecord.on('data', data => {
+        //   const chunk = Buffer.from(data, 'base64');
+        //   console.log('chunk size', chunk.byteLength);
+        //   // do something with audio chunk
+        // });
+    }
+
+    checkPermission = async () => {
+        const p = await Permissions.check('microphone');
+        console.log('permission check', p);
+        if (p === 'authorized') return;
+        return this.requestPermission();
+    };
+
+    requestPermission = async () => {
+        const p = await Permissions.request('microphone');
+        console.log('permission request', p);
+    };
+
+    start = () => {
+        console.log('start record');
+        this.setState({ audioFile: '', recording: true, loaded: false });
+        AudioRecord.start();
+    };
+
+    stop = async () => {
+        if (!this.state.recording) return;
+        console.log('stop record');
+        let audioFile = await AudioRecord.stop();
+        console.log('audioFile', audioFile);
+        this.setState({ audioFile, recording: false });   
+    };
+
+    load = () => {
+        return new Promise((resolve, reject) => {
+          if (!this.state.audioFile) {
+            return reject('file path is empty');
           }
-        }, 1000)
-    }
-
-    stopRecording = () => {
-        const { isRecording } = this.state
-        if (!isRecording) return
     
-        AudioRecorder.stopRecording()
-        this.setState({audioLength: this.state.currentTime + 1})
-        clearInterval(this.timer)
-        this.setState({ isRecording: false, isFinishRecorded: true, currentTime: 0})
-    }
+          this.sound = new Sound(this.state.audioFile, '', error => {
+            if (error) {
+              console.log('failed to load the file', error);
+              return reject(error);
+            }
+            this.setState({ loaded: true });
+            return resolve();
+          });
+        });
+      };
+    
+      play = async () => {
+        if (!this.state.loaded) {
+          try {
+            await this.load();
+          } catch (error) {
+            console.log(error);
+          }
+        }
+    
+        this.setState({ paused: false });
+        Sound.setCategory('Playback');
+    
+        this.sound.play(success => {
+          if (success) {
+            console.log('successfully finished playing');
+          } else {
+            console.log('playback failed due to audio decoding errors');
+          }
+          this.setState({ paused: true });
+          // this.sound.release();
+        });
+    };
 
     render() {
         return(
             <ScrollView style={styles.container} contentContainerStyle={{alignItems:'center'}}>
                 <Image style={styles.iconStyle} source={require('../images/logo.png')}></Image>
                 <Text style={styles.titleStyle}>Record</Text>
+                <RoundButton
+                    iconImage={require('../images/microphone.png')}
+                    onPress={() => {this.start()}}
+                    disabled={this.state.recording}>
+                </RoundButton>
+                <RoundButton
+                    iconImage={require('../images/stop.png')}
+                    onPress={() => {this.stop()}}
+                    disabled={!this.state.recording}>
+                </RoundButton>
+                <Button title={'Play'} onPress={this.play}></Button>
                 
             </ScrollView>
         );
